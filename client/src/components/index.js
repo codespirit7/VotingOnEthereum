5c0c1be0fde8d962380d196cc2925b732f2d3daf
Shivendra1702 <shivendrapratapjadia@gmail.com>
2023-03-10 13:36:06 +0530

    first commit by shivendra

---
+++ client/src/components/index.js
@@ -0,0 +1,6 @@
+export { default as Login } from "./login/Login";
+export { default as Verify } from "./verify/Verify";
+export { default as Aadhaar } from "./Aadhaar/Aadhaar";
+export { default as VotingPanel } from "./Voting_panel/VotingPanel";
+export { default as Success } from "./Success/Success";
+export { default as Missing } from "./Missing/Missing";
